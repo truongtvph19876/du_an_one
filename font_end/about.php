1a9aeecd3e24b4d559ad19394461a08450eb3c9d
TranMinhDuc2003 <106735778+TranMinhDuc2003@users.noreply.github.com>
2023-04-11 13:50:41 +0700

    hoan thanh about font end

---
+++ font_end/about.php
@@ -0,0 +1,103 @@
+
+<!--== End Page Breadcrumb ==-->
+
+<!--== Page Content Wrapper Start ==-->
+<div id="page-content-wrapper">
+    <div class="container">
+        <!-- About Page Content Start -->
+        <div class="row">
+            <!-- About Text Start -->
+            <div class="col-lg-6 order-last order-lg-first">
+                <div class="about-text-wrap">
+                    <h2><span>Cung cấp sản phẩm </span>tốt nhất cho bạn</h2>
+                    <p>Chúng tôi cung cấp dầu râu tốt nhất trên toàn thế giới. Chúng tôi là cửa hàng tốt nhất thế giới ở indi cho Beard
+                        Dầu. Bạn có thể mua sản phẩm của chúng tôi mà không có bất kỳ do dự nào vì họ tin tưởng chúng tôi và mua sản phẩm của chúng tôi
+                        không có bất kỳ sự nghi ngờ nào vì họ tin tưởng và luôn vui vẻ mua sản phẩm của chúng tôi.</p>
+
+                    <p>Một số khách hàng của chúng tôi nói rằng họ tin tưởng chúng tôi và mua sản phẩm của chúng tôi mà không chút do dự vì họ tin tưởng chúng tôi và luôn vui vẻ mua sản phẩm của chúng tôi.</p>
+
+                    <p>
+                        Chúng tôi cung cấp những gì tốt nhất mà họ đã tin tưởng và mua sản phẩm của chúng tôi mà không chút do dự vì họ tin tưởng chúng tôi và luôn vui vẻ mua hàng.</p>
+                </div>
+            </div>
+            <!-- About Text End -->
+
+            <!-- About Image Start -->
+            <div class="col-lg-5 col-md-10 m-md-auto ms-auto">
+                <div class="about-image-wrap">
+                    <img src="assets_fontend/img/about.jpg" alt="About"/>
+                </div>
+            </div>
+            <!-- About Image End -->
+        </div>
+        <!-- About Page Content End -->
+    </div>
+</div>
+<!--== Page Content Wrapper End ==-->
+<!--== End Brand Carousel Area ==-->
+
+<!--== Start Call To Action ==-->
+<section id="call-to-action" class="callto-action-two">
+    <div class="container">
+        <div class="call-to-action-content">
+            <div class="row">
+                <!-- Single Call to Action Start -->
+                <div class="col-lg-3 col-sm-6 m-auto">
+                    <div class="single-action-item d-flex align-items-center">
+                        <div class="call-action-icon">
+                            <i class="fa fa-truck"></i>
+                        </div>
+                        <div class="call-action-info">
+                            <h4>Miễn phí vận chuyển</h4>
+                            <p>Sản phẩm trên 10tr</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- Single Call to Action End -->
+
+                <!-- Single Call to Action Start -->
+                <div class="col-lg-3 col-sm-6">
+                    <div class="single-action-item d-flex align-items-center">
+                        <div class="call-action-icon">
+                            <i class="fa fa-support"></i>
+                        </div>
+                        <div class="call-action-info">
+                            <h4> Hỗ trọ 24/7</h4>
+                            <p>Tư vấn trực tuyến</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- Single Call to Action End -->
+
+                <!-- Single Call to Action Start -->
+                <div class="col-lg-3 col-sm-6">
+                    <div class="single-action-item d-flex align-items-center">
+                        <div class="call-action-icon">
+                            <i class="fa fa-calendar"></i>
+                        </div>
+                        <div class="call-action-info">
+                            <h4>Cập nhật hàng ngày</h4>
+                            <p>Kiểm tra cửa hàng</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- Single Call to Action End -->
+
+                <!-- Single Call to Action Start -->
+                <div class="col-lg-3 col-sm-6">
+                    <div class="single-action-item d-flex align-items-center">
+                        <div class="call-action-icon">
+                            <i class="fa fa-refresh"></i>
+                        </div>
+                        <div class="call-action-info">
+                            <h4>Hoàn trả trong vòng 15 ngày</h4>
+                            <p>Đảm bảo hoàn tiền</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- Single Call to Action End -->
+            </div>
+        </div>
+    </div>
+</section>
+<!--== End Call To Action ==-->
